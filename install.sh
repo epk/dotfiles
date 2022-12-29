cd6b79b65ccd17dbe2759d59b91b3e568acea91b
Aditya Sharma <git@adi.run>
2022-12-29 15:34:38 -0800

    install.sh

--- install.sh
+++ install.sh
@@ -4,27 +4,65 @@
 # -u: exit on unset variables
 set -eu
 
+log_color() {
+  color_code="$1"
+  shift
+
+  printf "\033[${color_code}m%s\033[0m\n" "$*" >&2
+}
+
+log_red() {
+  log_color "0;31" "$@"
+}
+
+log_blue() {
+  log_color "0;34" "$@"
+}
+
+log_task() {
+  log_blue "🔃" "$@"
+}
+
+log_error() {
+  log_red "❌" "$@"
+}
+
+error() {
+  log_error "$@"
+  exit 1
+}
+
 if ! chezmoi="$(command -v chezmoi)"; then
   bin_dir="${HOME}/.local/bin"
   chezmoi="${bin_dir}/chezmoi"
-  echo "Installing chezmoi to '${chezmoi}'" >&2
+  log_task "Installing chezmoi to '${chezmoi}'"
   if command -v curl >/dev/null; then
-    chezmoi_install_script="$(curl -fsSL get.chezmoi.io)"
+    chezmoi_install_script="$(curl -fsSL https://get.chezmoi.io)"
   elif command -v wget >/dev/null; then
-    chezmoi_install_script="$(wget -qO- get.chezmoi.io)"
+    chezmoi_install_script="$(wget -qO- https://get.chezmoi.io)"
   else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
+    error "To install chezmoi, you must have curl or wget."
   fi
   sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
   unset chezmoi_install_script bin_dir
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+# shellcheck disable=SC2312
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
+set -- init --source="${script_dir}"
 
-echo "Running 'chezmoi $*'" >&2
-# exec: replace current process with chezmoi
-exec "$chezmoi" "$@"
+if [ -n "${DOTFILES_ONE_SHOT-}" ]; then
+  set -- "$@" --one-shot
+else
+  set -- "$@" --apply
+fi
+
+if [ -n "${DOTFILES_DEBUG-}" ]; then
+  set -- "$@" --debug
+fi
+
+log_task "Running 'chezmoi $*'"
+# replace current process with chezmoi
+exec "${chezmoi}" "$@"
