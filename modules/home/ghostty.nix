30cb527439e9689233ced48ba7c4803e4fd1f8da
Aditya Sharma <aditya.sharma@shopify.com>
2026-08-31 22:41:31 -0400

    ghostty tip
    Signed-off-by: Aditya Sharma <aditya.sharma@shopify.com>

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -5,6 +5,10 @@
     package = null;
     enableZshIntegration = true;
     settings = {
+      # The cask is auto_updates, so Homebrew leaves upgrades to Sparkle. Point
+      # Sparkle at tip to track main-branch builds instead of tagged releases.
+      auto-update = "download";
+      auto-update-channel = "tip";
       cursor-style = "block";
       cursor-style-blink = true;
       bold-is-bright = true;
